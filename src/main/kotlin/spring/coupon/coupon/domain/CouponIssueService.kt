154732b617668baebcda8482c64c9fe69fd8e614
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-11 16:39:52 +0900

    feat: 분산락 테스트를 작성한다.

--- src/main/kotlin/spring/coupon/coupon/domain/CouponIssueService.kt
+++ src/main/kotlin/spring/coupon/coupon/domain/CouponIssueService.kt
@@ -1,9 +1,8 @@
 package spring.coupon.coupon.domain
 
+import jakarta.transaction.Transactional
 import org.redisson.api.RedissonClient
 import org.springframework.stereotype.Service
-import org.springframework.transaction.PlatformTransactionManager
-import org.springframework.transaction.support.TransactionTemplate
 import spring.coupon.core.exception.error.GlobalErrorCode
 import spring.coupon.coupon.exception.CouponErrorCode
 import java.util.concurrent.TimeUnit
@@ -13,8 +12,8 @@ class CouponIssueService(
     private val couponRepository: CouponRepository,
     private val couponCountRepository: CouponCountRepository,
     private val redissonClient: RedissonClient,
-    private val transactionManager: PlatformTransactionManager
 ) {
+    @Transactional
     fun issueCoupon(userId: Long): Coupon? {
 
         val lock = redissonClient.getLock("COUPON_ISSUE_DISTRIBUTED_LOCK")
@@ -23,26 +22,19 @@ class CouponIssueService(
             throw GlobalErrorCode.FAILED_TO_ACQUIRE_LOCK.toException()
         }
 
-        try {
-            return TransactionTemplate(transactionManager).execute {
-                couponRepository.findByUserId(userId)?.let {
-                    return@execute it
-                }
-
-                val couponCount = couponCountRepository.count() ?: 0
-
-                if (couponCount >= 100) {
-                    throw CouponErrorCode.ALL_COUPON_ISSUED.toException()
-                }
-
-                couponCountRepository.increment()
-
-                return@execute couponRepository.save(Coupon.create(userId))
-            }
-        } finally {
-            if (lock.isHeldByCurrentThread) {
-                lock.unlock()
-            }
+        couponRepository.findByUserId(userId)?.let {
+            return it
         }
+
+        val couponCount = couponCountRepository.count() ?: 0
+
+        if (couponCount >= 100) {
+            throw CouponErrorCode.ALL_COUPON_ISSUED.toException()
+        }
+
+        couponCountRepository.increment()
+        lock.unlock()
+
+        return couponRepository.save(Coupon.create(userId))
     }
 }
