d3e3d2d4985442281e363ec10eb2f83b7c74d8a1
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-11 16:19:23 +0900

    feat: 쿠폰 생성 시 비관락을 적용한다.

--- src/main/kotlin/spring/coupon/coupon/domain/CouponRepository.kt
+++ src/main/kotlin/spring/coupon/coupon/domain/CouponRepository.kt
@@ -1,7 +1,15 @@
 package spring.coupon.coupon.domain
 
+import jakarta.persistence.LockModeType
+import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.QueryHints
 
-interface CouponRepository : JpaRepository<Coupon, String>{
+interface CouponRepository : JpaRepository<Coupon, Int>{
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(
+        QueryHint(name = "jakarta.persistence.lock.timeout", value = "5000")
+    )
     fun findByUserId(userId: Long): Coupon?
 }
