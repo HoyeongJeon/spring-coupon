5f2a57c6596a166c18979af9210eb73136e66d00
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-11 16:20:56 +0900

    chore: k6 테스트 스크립트를 작성한다.

---
+++ k6.js
@@ -0,0 +1,44 @@
+import http from 'k6/http';
+import {check} from 'k6';
+
+export const options = {
+    scenarios: {
+        coupon_issue: {
+            executor: 'ramping-vus',
+            startVUs: 0,
+            stages: [
+                { duration: '1s', target: 200 },
+                { duration: '30s', target: 600 },
+                { duration: '1s', target: 0 }
+            ],
+        },
+    },
+};
+
+export default function () {
+    const userId = __VU;
+    const payload = JSON.stringify({
+        userId
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const res = http.post('http://localhost:8080/api/v1/coupon/issue', payload, params);
+
+    if (res.status !== 200 && res.status !== 404) {
+        non200Or404Counter.add(1);
+        console.log(`예상 외 응답: ${res.status}, userId: ${userId}, 응답 내용: ${res.body}`);
+    }
+
+    if (res.status === 200 || res.status === 404) {
+        console.log(`성공: ${res.status}, userId: ${userId}, 응답 내용: ${res.body}`);
+    }
+
+    check(res, {
+        '정상 응답 (200) 또는 품절 (404)': (r) => r.status === 200 || r.status === 404,
+    });
+}
