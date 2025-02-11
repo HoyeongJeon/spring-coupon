5c247b9f7d81070b2c95086c9b600a9bca822808
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-11 16:39:35 +0900

    feat: CouponService의 @Transactional을 제거한다.

--- src/main/kotlin/spring/coupon/coupon/application/CouponService.kt
+++ src/main/kotlin/spring/coupon/coupon/application/CouponService.kt
@@ -1,6 +1,5 @@
 package spring.coupon.coupon.application
 
-import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import spring.coupon.coupon.domain.CouponIssueService
 import spring.coupon.coupon.presentation.dto.request.CouponIssueRequest
@@ -10,8 +9,7 @@ import spring.coupon.coupon.presentation.dto.response.CouponIssueResponse
 class CouponService(
     private val couponIssueService: CouponIssueService
 ) {
-    @Transactional
     fun issueCoupon(couponIssueRequest: CouponIssueRequest): CouponIssueResponse {
-        return CouponIssueResponse(couponIssueService.issueCoupon(couponIssueRequest.userId).id!!)
+        return CouponIssueResponse(couponIssueService.issueCoupon(couponIssueRequest.userId)?.id!!)
     }
 }
